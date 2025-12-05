5f9b008f6ee7a41f81f425f8c43b24daa945e448
Oleksii Savchenko <lab108uav@gmail.com>
2025-12-05 16:17:39 +0200

    Add input URL Youtube

--- youtube_fix/main.py
+++ youtube_fix/main.py
@@ -16,7 +16,7 @@ try:
     # Get the lowest resolution stream
     video_stream = yt.streams.get_lowest_resolution()
 
-    # Download the video
+    # Download the video from
     print(f"Downloading: {yt.title}")
     video_stream.download(output_path="E:/Video/Youtube/")
     print("--> E:/Video/Youtube/...\n","Download complete!" )
